1c02356f1ce9b7ce15324076fdb7a0ff88b5e617
hsx-lghh0812 <zhang.renwu@successinfo.com.cn>
2017-05-08 17:47:46 +0800

    commit

--- src/webapp/index/index.js
+++ src/webapp/index/index.js
@@ -2,13 +2,15 @@
  * Created by hsx on 2017/5/8.
  */
 import React, { Component } from 'react';
-
-
+import "../../plugins/bootstrap-4.0.0-alpha.6/dist/css/bootstrap.css"
+import {Modal, Button} from "react-bootstrap";
+ 
 class index extends Component {
     render() {
         return (
             <div>
               index page;
+                <Button bsSize="small" bsStyle="success">submit</Button>
             </div>
         );
     }
