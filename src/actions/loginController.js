94f304dab060663258715df7c3082ab9f095684f
hsx-lghh0812 <zhang.renwu@successinfo.com.cn>
2017-05-11 23:08:04 +0800

    commit

--- src/actions/loginController.js
+++ src/actions/loginController.js
@@ -2,9 +2,9 @@
  *
  */
 import {host} from "../config/config"
-export const login = (username, password) => (dispatch) => {
+export const login = (username, password, call) => (dispatch) => {
     console.log(username, password);
-    return fetch(host + "login/login_in", {
+    fetch(host + "login/login_in", {
         method: "POST",
         mode: "cors",
         headers: {
@@ -19,6 +19,52 @@ export const login = (username, password) => (dispatch) => {
                 type: "login",
                 data
             });
+
+            call(data);
+        }
+    });
+}
+export const email = (email, call) => (dispatch) => {
+    console.log(email);
+    fetch(host + "login/send_email", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: "email=" + email
+    }).then(res => res.json()).then(data => {
+        console.log(data, data.code);
+        if (data.code == 1001) {
+            console.log("1001");
+            dispatch({
+                type: "email",
+                data
+            });
+
+            call(data);
+        }
+    });
+}
+export const register = (username, password, email, verification, call) => (dispatch) => {
+    console.log(email);
+    fetch(host + "login/register", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: "username=" + username + "&password=" + password + "&email=" + email + "&verification=" + verification
+    }).then(res => res.json()).then(data => {
+        console.log(data, data.code);
+        if (data.code == 1001) {
+            console.log("1001");
+            dispatch({
+                type: "email",
+                data
+            });
+
+            call(data);
         }
     });
 }
--- src/webapp/login/Login.js
+++ src/webapp/login/Login.js
@@ -8,6 +8,8 @@ import "../../plugins/font-awesome-4.7.0/css/font-awesome.css"
 import "../../index.css"
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
+import {Redirect} from 'react-router'
+import {Link} from 'react-router-dom'
 import * as loginAction from "../../actions/loginController"
 import md5 from "js-md5"
 class Login extends Component {
@@ -18,17 +20,44 @@ class Login extends Component {
 
     handleSubmit(username, password) {
         console.log("username: " + username, "password:" + password, this)
-        this.props.loginAction.login(username, password);
+        this.props.loginAction.login(username, password, this.callback.bind(this));
+    }
+
+    callback(data) {
+        console.log(data);
+        this.props.history.push("/index");
+    }
+
+    redirect() {
+        this.props.history.push("/user/register");
     }
 
     render() {
-        console.log(this.props.router)
-        return (
-            <LoginPosition>
-                <LoginPanel {...this.props}
-                            submit={(name, pass) => this.handleSubmit(name, pass)}/>
-            </LoginPosition>
-        );
+        console.log(this);
+        switch (this.props.match.params.type) {
+            case "login":
+                return (
+                    <LoginPosition>
+                        <LoginPanel {...this.props}
+                                    submit={(name, pass) => this.handleSubmit(name, pass)}/>
+                    </LoginPosition>
+                );
+            case "register":
+                return (
+                    <LoginPosition>
+                        <RegisterPanel {...this.props}
+                                       submit={(name, pass) => this.handleSubmit(name, pass)}/>
+                    </LoginPosition>
+                );
+            case "reset":
+                return (
+                    <LoginPosition>
+                        <ResetPanel {...this.props}
+                                    submit={(name, pass) => this.handleSubmit(name, pass)}/>
+                    </LoginPosition>
+                );
+        }
+
     }
 }
 
@@ -46,15 +75,15 @@ export default connect(
     mapDispatchToProps
 )(Login);
 
-
 class LoginPosition extends Component {
 
     constructor(props) {
         super(props);
+        // type : this.props.type.isRequired
     }
 
     render() {
-        console.log(this);
+        console.log(this, this.props);
         return (
             <div className="fixed-center">
                 <div className="container" style={{height: "75%"}}>
@@ -67,6 +96,7 @@ class LoginPosition extends Component {
             </div>);
     }
 }
+
 
 class LoginPanel extends Component {
 
@@ -141,12 +171,93 @@ class LoginPanel extends Component {
                     </form>
 
                     <div className="row">
-                        <div className="d-flex justify-content-between w-100">
-                            <button className="btn btn-link">注册</button>
-                            <button className="btn btn-link">忘记密码</button>
+                        <div className="col">
+                            <div className="d-flex justify-content-between w-100">
+                                <Link to="/user/register">注册</Link>
+                                <Link to="/user/reset">忘记密码</Link>
+                            </div>
                         </div>
                     </div>
-                    <button className="btn btn-primary w-100" onClick={this.submit.bind(this)}>
+                    <div className="row">
+                        <div className="col">
+                            <button className="btn btn-primary w-100" onClick={this.submit.bind(this)}>
+                                login in
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+class RegisterPanel extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    register() {
+        console.log(this)
+        this.props.loginAction.register(this.refs.r_username.value, this.refs.r_password.value,
+            this.refs.r_email.value, this.refs.r_code.value, data => {
+                console.log(data)
+            }
+        )
+        ;
+    }
+
+    sendMail() {
+        console.log(this)
+        this.props.loginAction.email(this.refs.r_email.value, data => {
+            console.log(data)
+        });
+    }
+
+
+    render() {
+        console.log(this)
+        return (
+            <div className="card">
+                <div className="card-header">注册</div>
+                <div className="card-block">
+                    <form>
+                        <div className="input-group">
+                                        <span className="input-group-addon" id="r_basic-addon1">
+                                            <span className="fa fa-user-o w14 f14"></span>
+                                        </span>
+                            <input type="text" className="form-control" placeholder="username"
+                                   ref="r_username" aria-describedby="r_basic-addon1"/>
+                        </div>
+                        <br/>
+                        <div className="input-group">
+                                        <span className="input-group-addon" id="r_asic-addon2">
+                                        <span className="fa fa-lock w14 f14"></span>
+                                        </span>
+                            <input type="password" className="form-control" placeholder="password"
+                                   ref="r_password" aria-describedby="r_basic-addon2"/>
+                        </div>
+                        <br/>
+                        <div className="input-group">
+                                        <span className="input-group-addon" id="r_asic-addon4">
+                                        <span className="fa fa-lock w14 f14"></span>
+                                        </span>
+                            <input type="email" className="form-control" placeholder="email"
+                                   ref="r_email" aria-describedby="r_basic-addon4"/>
+                        </div>
+                        <br/>
+                        <div className="input-group">
+                            <input type="text" className="form-control" placeholder="text"
+                                   ref="r_code" aria-describedby="r_basic-addon3"/>
+                            <span className="input-group-btn" id="r_basic-addon3">
+                                         <button className="btn btn-secondary" type="button"
+                                                 onClick={this.sendMail.bind(this)}>获取验证码</button>
+                            </span>
+                        </div>
+                        <br/>
+                    </form>
+                    <button className="btn btn-primary w-100" onClick={this.register.bind(this)}>
                         login in
                     </button>
                 </div>
@@ -156,5 +267,54 @@ class LoginPanel extends Component {
 }
 
 
+class ResetPanel extends Component {
 
+    constructor(props) {
+        super(props);
+    }
 
+    reset() {
+        console.log(this)
+        this.props.submit(this.refs.username.value, md5(this.refs.password.value));
+    }
+
+    render() {
+        console.log(this)
+        return (
+            <div className="card">
+                <div className="card-header">重置密码</div>
+                <div className="card-block">
+                    <form>
+                        <div className="input-group">
+                                        <span className="input-group-addon" id="re_basic-addon1">
+                                            <span className="fa fa-user-o w14 f14"></span>
+                                        </span>
+                            <input type="text" className="form-control" placeholder="请使用邮箱号登录"
+                                   ref="re_username" aria-describedby="re_basic-addon1"/>
+                        </div>
+                        <br/>
+                        <div className="input-group">
+                                        <span className="input-group-addon" id="re_asic-addon2">
+                                        <span className="fa fa-lock w14 f14"></span>
+                                        </span>
+                            <input type="password" className="form-control" placeholder="password"
+                                   ref="re_password" aria-describedby="re_basic-addon2"/>
+                        </div>
+                        <br/>
+                        <div className="input-group">
+                                        <span className="input-group-addon" id="re_basic-addon3">
+                                        <span className="fa fa-lock w14 f14"></span>
+                                        </span>
+                            <input type="text" className="form-control" placeholder="password"
+                                   ref="re_email" aria-describedby="re_basic-addon3"/>
+                        </div>
+                        <br/>
+                    </form>
+                    <button className="btn btn-primary w-100" onClick={this.reset.bind(this)}>
+                        login in
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
