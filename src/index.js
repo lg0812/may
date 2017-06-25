6dee5a0df5e27b15602d9e5b7b00456af0695d80
LG0812 <zhang.renwu@successinfo.com.cn>
2017-06-25 22:43:02 +0800

    html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
-
-import {Router, Route, Redirect, BrowserRouter} from 'react-router-dom'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
 import reducers from './reducers'
 import thunk from "redux-thunk"
 import logger from "redux-logger"
@@ -12,7 +10,13 @@ import App from './webapp/app/App';
 import index from "./webapp/index/Index"
 import login from "./webapp/login/Login"
 import './index.css';
-const store = applyMiddleware(thunk, logger)(createStore)(reducers);
+// const store = applyMiddleware(thunk, logger)(createStore)(reducers);
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunk, logger)
+)
+
 /* 在控制台打印一条有样式的文字 */
 console.log("%c May %c Copyright \xa9 2016-%s",
     'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:64px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
@@ -21,13 +25,11 @@ console.log("%c May %c Copyright \xa9 2016-%s",
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter >
-            <div>
-                <Route path="/" exact component={App}/>
-                <Route path="/index" component={index}/>
-                <Route path="/user" exact component={login}/>
-            </div>
-        </BrowserRouter>
+        <Router >
+            <Route path="/" exact component={App}/>
+            <Route path="/index" component={index}/>
+            <Route path="/user" exact component={login}/>
+        </Router>
     </Provider >,
     document.getElementById('root')
 )
