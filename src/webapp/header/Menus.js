4c384f7eea58589afc2c4293cfa6d9f195a4061c
LG0812 <zhang.renwu@successinfo.com.cn>
2017-07-25 22:56:59 +0800

    menus

--- src/webapp/header/Menus.js
+++ src/webapp/header/Menus.js
@@ -3,58 +3,46 @@
  */
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import menus from "./menus.json";
 class Menus extends Component {
     constructor(props) {
         super(props)
-        console.log(this.props.lang, ">>>>>>>>")
+        let menuArr = [];
         this.state = {
-            lang: this.props.lang,
-            menuArr: [{
-                name: this.state.lang.group1,
-                isOpen: false,
-                subList: [{name: "云服务器", href: "/auth/basic"}, {name: "云数据库", href: "/auth/basic1"}, {
-                    name: "云数据库",
-                    href: "/auth/basic1"
-                }]
-            }, {
-                name: "安全", isOpen: false,
-                subList: [{name: "web应用防火墙", href: "bbb-href"}, {name: "安全管家", href: "bbb-href"}]
-            }, {
-                name: "云市场", isOpen: false,
-                subList: [{name: "云市场", href: "ccc-href"}, {name: "场景编辑", href: "ccc-href"}, {
-                    name: "云数据库",
-                    href: "/auth/basic1"
-                }, {name: "云数据库", href: "/auth/basic1"}]
-            }]
+            openStatus: []
         }
     }
 
     toggle(i) {
-        console.log(i, "toggle----->");
-        let temp = this.state.menuArr;
-        for (var t = 0; t < temp.length; t++) {
+        let temp = this.state.openStatus;
+        for (var t = 0; t < this.menuArr.length; t++) {
+            if (!temp[t])
+                temp[t] = {};
             if (t != i) {
                 temp[t].isOpen = false;
+            } else {
+                temp[t].isOpen = !temp[t].isOpen;
             }
         }
-        temp[i].isOpen = !temp[i].isOpen;
-        this.setState({menuArr: temp});
+
+        this.setState({openStatus: temp});
     }
 
     render() {
+        this.menuArr = menus[this.props.lang.type];
         return (
-            <div className="menus text-center self-hover">
+            <div className="menus text-center self-hover" style={{"minWidth":"180px"}}>
                 <ul className="flex-column w-100 h-100 pd0" style={{"lineHeight": "40px"}}>
                     {
-                        this.state.menuArr.map((obj, index) => {
+                        this.menuArr.map((obj, index) => {
                             return (<li key={index} className="fc-white super_menus">
                                 <div onClick={() => this.toggle(index)}><span
-                                    className={"fa fa-angle-down mgr10 transform-rotate " + (obj.isOpen ? "transform90" : "transform-90") }
+                                    className={"fa fa-angle-down mgr10 transform-rotate " + (this.state.openStatus[index] && this.state.openStatus[index].isOpen ? "transform90" : "transform-90") }
                                     style={{"fontSize": "16px"}}></span>{obj.name}
                                 </div>
                                 <ul className="flex-column  w-100 pd0 transform-height"
-                                    style={{height: (obj.isOpen ? (obj.subList.length * 40 + "px") : "0px")}}>
+                                    style={{height: (this.state.openStatus[index] && this.state.openStatus[index].isOpen ? (obj.subList.length * 40 + "px") : "0px")}}>
                                     {
                                         obj.subList.map((item, i) => {
                                             return (
