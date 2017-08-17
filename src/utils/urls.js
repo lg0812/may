a5b4c770c8b112ca3b3889ddeac39e103632f19f
LG0812 <zhang.renwu@successinfo.com.cn>
2017-08-17 20:18:58 +0800

    port

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -20,7 +20,7 @@ const urls = {
     "public_reset": "/january/public/reset",
     "public_register": "/january/public/register",
     // "home_page_url": "http://localhost:8080/january/homepage.html",
-    "home_page_url": "http://106.14.136.160/january/homepage.html"
+    "home_page_url": "http://106.14.136.160:8080/january/homepage.html"
 }
 
 export {urls};
