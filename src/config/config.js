b4bb865c05ba412b54c48e71e6813211e3fe3127
LG0812 <zhang.renwu@successinfo.com.cn>
2017-08-27 12:08:09 +0800

    none

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@
  * Created by hsx on 2017/5/10.
  */
 
-// export const host = "http://106.14.136.160:8080/january/";
-export const host = "http://127.0.0.1:8080/january/";
+export const host = "http://106.14.136.160:8080/january/";
+// export const host = "http://127.0.0.1:8080/january/";
 export const requestMapping = {
     login: "login/login_in",
     send_email: "login/send_email",
