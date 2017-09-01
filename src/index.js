094deff5002adf17a658063c37dde37dcf7d0875
LG0812 <2356581512@qq.com>
2017-09-01 15:51:08 +0800

    all

--- src/index.js
+++ src/index.js
@@ -113,4 +113,3 @@ ReactDOM.render(
 
 
 
-
--- src/webapp/container/Basic.js
+++ src/webapp/container/Basic.js
@@ -7,9 +7,23 @@ class Basic extends Component {
     render() {
         return (
             <div className="container">
-                basic
+                <div style={{
+                    width: "200px",
+                    height: "200px",
+                    marginTop: "100px",
+                    position: "relative",
+                    border: "1px solid rgba(0,0,0,.25)",
+                    borderRadius:"6px"
+                }}>
+                    <div className="pure-tarrow"></div>
+                    <div className="pure-rarrow"></div>
+                    <div className="pure-larrow"></div>
+                    <div className="pure-barrow"></div>
+                </div>
+                <div style={{marginTop:"20px"}} className="right-arrow"></div>
             </div>
         );
     }
 }
+
 export default Basic;
