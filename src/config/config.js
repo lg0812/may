459817cb14ef2249811acb1b4753d335dfaecb87
LG0812 <2356581512@qq.com>
2017-09-06 17:09:34 +0800

    all

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@
  * Created by hsx on 2017/5/10.
  */
 
-// export const host = "http://www.yltfy.cn/january/";
-export const host = "http://127.0.0.1:8081/january/";
+export const host = "http://www.yltfy.cn/january/";
+// export const host = "http://127.0.0.1:8081/january/";
 export const requestMapping = {
     login: "login/login_in",
     send_email: "login/send_email",
